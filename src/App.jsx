3e1dee27ad7c05e094dda2225c5b2c75bee4d691
tareqsazid <tareqszd007@gmail.com>
2023-04-08 12:58:48 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header/Header";
+
 function App() {
   return (
-    <div className='App'>
-      <h1>Welcome to nextPage!</h1>
+    <div>
+      <Header></Header>
+      <Outlet></Outlet>
+      {/* Footer  */}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,37 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Books from "./components/Books";
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "books",
+        element: <Books></Books>,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+    ],
+  },
+  {
+    path: "/about",
+    element: <p>About page</p>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
